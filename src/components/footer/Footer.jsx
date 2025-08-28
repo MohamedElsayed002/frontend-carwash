4ddb47d9e5754d781eadf0963d322d79c98c6faf
MohammedHamed1 <car.washer.email@gmail.com>
2025-08-29 00:44:08 +0300

    commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
               <Link className="group" to="/">
                 <div className="relative">
                   <div className="w-12 h-12 sm:w-16 sm:h-16 lg:w-20 lg:h-20 rounded-xl lg:rounded-2xl bg-gradient-to-br from-white to-gray-100 flex items-center justify-center border border-white/50 shadow-xl hover:shadow-2xl transition-all duration-300 group-hover:scale-105">
-                    <img alt="PayPass Logo" className="w-7 h-7 sm:w-10 sm:h-10 lg:w-12 lg:h-12 object-contain" src="/src/assets/logo.png" />
+                    <img alt="PayPass Logo" className="w-7 h-7 sm:w-10 sm:h-10 lg:w-12 lg:h-12 object-contain" src="/public/assets/logo.png" />
                   </div>
                   <div className="absolute -top-1 -right-1 lg:-top-1.5 lg:-right-1.5 w-4 h-4 lg:w-6 lg:h-6 bg-gray-500 rounded-full flex items-center justify-center shadow-lg border-2 border-white">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-phone w-2 h-2 lg:w-3 lg:h-3 text-white" aria-hidden="true">
